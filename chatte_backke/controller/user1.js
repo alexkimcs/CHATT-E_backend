577d77086ad5af3d3600b343fb36f563c3bcb524
ls <cancel>
2021-09-03 15:15:22 -0500

    user2 controller

--- chatte_backke/controller/user1.js
+++ chatte_backke/controller/user1.js
@@ -14,7 +14,21 @@ router.get('/user1/:id', (req, res, next) => {
     .catch(next)
 })
 
-router.post('/', (req, res, next) => {
+router.post('/user1', (req, res, next) => {
     User1.create(req.body)
     .then(person_1 => {res.json(person_1)})
 })
+
+router.put('/user1/:id', (req, res, next) => {
+    User1.findOneAndUpdate({_id: req.params.id}, req.body, {new: true})
+    .then(person_1 => res.json(person_1))
+    .catch(next)
+})
+
+router.delete('/user1/:id', (req, res, next) => {
+    User1.findOneAndDelete({_id: req.params.id})
+    .then(person_1 => res.json(person_1))
+    .catch(next)
+})
+
+module.exports = router
