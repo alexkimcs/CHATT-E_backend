3995de3b997f2f0f78ddc0be34ca88139965b4b3
ls <cancel>
2021-09-06 16:25:36 -0500

    server and updated controller route

--- chatte_backke/controller/user1.js
+++ chatte_backke/controller/user1.js
@@ -2,30 +2,30 @@ const express = require('express')
 const router = express.Router()
 const User1 = require('../db/models/user1')
 
-router.get('/user1' , (req, res, next) => {
+router.get('/' , (req, res, next) => {
     User1.find({})
     .then(person_1 => res.json(person_1))
     .catch(next)
 })
 
-router.get('/user1/:id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
     User1.find({_id: req.params.id})
     .then(person_1 => res.json(person_1))
     .catch(next)
 })
 
-router.post('/user1', (req, res, next) => {
+router.post('/', (req, res, next) => {
     User1.create(req.body)
     .then(person_1 => {res.json(person_1)})
 })
 
-router.put('/user1/:id', (req, res, next) => {
+router.put('/:id', (req, res, next) => {
     User1.findOneAndUpdate({_id: req.params.id}, req.body, {new: true})
     .then(person_1 => res.json(person_1))
     .catch(next)
 })
 
-router.delete('/user1/:id', (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
     User1.findOneAndDelete({_id: req.params.id})
     .then(person_1 => res.json(person_1))
     .catch(next)
--- chatte_backke/server.js
+++ chatte_backke/server.js
@@ -1,2 +1,19 @@
 const express = require('express')
-const app = express()
+const user1Controller = require('./controller/user1`');
+
+//app config
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+//Controllers go here
+app.get('/user1', user1Controller)
+app.use('/user1', user1Controller)
+
+
+//local port
+app.get('/', (req, res) => {
+    res.send('Chatt-E')
+})
+const port = 4000
+app.listen(port, () => {console.log(`running on port ${port}`)})
