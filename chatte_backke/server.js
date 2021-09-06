23a369bbdbf0e4e9a19063efdd0e934b24cfa7c6
ls <cancel>
2021-09-06 16:28:31 -0500

    cors install and server update

--- chatte_backke/server.js
+++ chatte_backke/server.js
@@ -1,12 +1,14 @@
 const express = require('express')
-const user1Controller = require('./controller/user1`');
+const cors = require('cors')
 
 //app config
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 //Controllers go here
+const user1Controller = require('./controller/user1`');
 app.get('/user1', user1Controller)
 app.use('/user1', user1Controller)
 
