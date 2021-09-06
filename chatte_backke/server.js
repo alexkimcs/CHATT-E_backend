0d5bf77bf78ca7f0ef08036fa8e23c47984fdf4a
alexkimcs <alexkimfit@gmail.com>
2021-09-06 16:43:59 -0600

    backend testing with sam

--- chatte_backke/server.js
+++ chatte_backke/server.js
@@ -1,21 +1,23 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
 
 //app config
 const app = express();
-app.use(cors())
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+const port = process.env.PORT || 4000;
 
 //Controllers go here
-const user1Controller = require('./controller/user1`');
-app.get('/user1', user1Controller)
-app.use('/user1', user1Controller)
+// const user1Controller = require('./controller/user1`');
+// app.get('/user1', user1Controller)
+// app.use('/user1', user1Controller)
 
 
 //local port
 app.get('/', (req, res) => {
     res.send('Chatt-E')
 })
-const port = 4000
+
+
+//LISTEN    
 app.listen(port, () => {console.log(`running on port ${port}`)})
