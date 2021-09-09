63c0d038bf463f29c93320066ee0d4ce869cb64f
ls <cancel>
2021-09-09 14:20:23 -0500

    server.js - Message.find

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const io = require('socket.io')(5000, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         method: ['GET', 'POST']
     }
 });
@@ -12,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({entended: true}));
 
+//Schemas
+const User = require('./db/models/user');
+const Message = require('./db/models/message');
 
 //Controllers go here
 const userController = require('./controller/userController');
@@ -26,19 +29,26 @@ app.get('/', (req,res) => {
 
 //io
 io.on("connection", socket => {
-    console.log('userIO.on connected');
+    Message.find().then( data => {
+        socket.emit('message', data)
+    })
+
+    console.log('user connected');
     // socket.emit('message', 'hello from backend');
     socket.on('disconnect', () => {
         console.log('user disconnected')
     })
-    socket.on('chatMessage', msg => {
-        io.emit('message', msg)
+    socket.on('chatMessage', (msg) => {
+        // const message = new Message({msg});
+        // message.save().then(() => io.emit('message', msg) )
+        io.emit('message', {
+            name: socket.id,
+            message: msg
+        })
     })
 });
 
-app.set('port', process.env.PORT || 4000)
-
-
+app.set('port', process.env.PORT || 4001)
 
 //LISTEN    
 app.listen(app.get('port'), () => {
